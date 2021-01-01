9c43147769efc38f2c3a863d216bc70ac2a7f005
Fábio Alves <fabioalves.contacto@gmail.com>
2021-01-01 08:39:35 -0100

    ADD: Route GET/PUT

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const {uuid} = require("uuidv4")
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
@@ -11,15 +11,25 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, tech} = request.body
+  const repo = {id:uuid(), title, url, tech, likes:0}
+  repositories.push(repo)
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const {title, url, tech} = request.body
+  const repo = {
+    title, url, tech
+  }
+  const repoIndex = repositories.findIndex(repo => repositories.id === id)
+  repositories[repoIndex] = repo
+  return response.json(repo)
 });
 
 app.delete("/repositories/:id", (request, response) => {
